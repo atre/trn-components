53b3f9f5de2d78b8ad40791ec96ba043a5b6f032
Mykyta Herashchenko <1991224+atre@users.noreply.github.com>
2023-11-27 16:31:21 +0100

    add collab

--- lib/github/repository.ts
+++ lib/github/repository.ts
@@ -2,13 +2,32 @@ import {
   Repository,
   RepositoryConfig,
 } from "@cdktf/provider-github/lib/repository";
+import { RepositoryCollaborator } from "@cdktf/provider-github/lib/repository-collaborator";
 
 import { Construct } from "constructs";
 
+export interface GithubRepositoryProps {
+  collaborator: {
+    username: string;
+    permission: string;
+  };
+}
+
 export class GithubRepository extends Construct {
-  constructor(scope: Construct, id: string, props: RepositoryConfig) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: RepositoryConfig & GithubRepositoryProps
+  ) {
     super(scope, id);
 
-    new Repository(this, "init-repo", props);
+    const { collaborator } = props;
+
+    const repo = new Repository(this, "init-repo", props);
+
+    new RepositoryCollaborator(this, "colab", {
+      ...collaborator,
+      repository: repo.name,
+    });
   }
 }
