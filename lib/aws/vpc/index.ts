8e6c085fda5ec18acb311105200ed2caf1d581b2
Serhii Kaliuzhnyi <kayuzhni.sergei@gmail.com>
2023-11-29 16:52:54 +0200

    Refactor options and import of aws provider

--- lib/aws/vpc/index.ts
+++ lib/aws/vpc/index.ts
@@ -1,4 +1,11 @@
-import * as aws from '@cdktf/provider-aws';
+import { Eip } from '@cdktf/provider-aws/lib/eip';
+import { InternetGateway } from '@cdktf/provider-aws/lib/internet-gateway';
+import { MainRouteTableAssociation } from '@cdktf/provider-aws/lib/main-route-table-association';
+import { NatGateway } from '@cdktf/provider-aws/lib/nat-gateway';
+import { RouteTable } from '@cdktf/provider-aws/lib/route-table';
+import { RouteTableAssociation } from '@cdktf/provider-aws/lib/route-table-association';
+import { Subnet } from '@cdktf/provider-aws/lib/subnet';
+import { Vpc } from '@cdktf/provider-aws/lib/vpc';
 import { Fn } from 'cdktf';
 import { Construct } from 'constructs';
 import { DEFAULT_CIDR } from './constant';
@@ -8,33 +15,35 @@ export class VpcConstruct extends Construct {
   constructor(scope: Construct, id: string, private opts: VpcOptions = {}) {
     super(scope, id);
 
-    const vpc = new aws.vpc.Vpc(this, 'vpc',
-      {
-        tags: { Name: opts?.name ?? '' },
-        cidrBlock: opts?.cidr ?? DEFAULT_CIDR,
-      });
+
+    const { name = '', cidr = DEFAULT_CIDR, azs, tags, publicSubnetTags, privateSubnetTags } = opts;
+
+    const vpc = new Vpc(this, 'vpc', {
+      tags: { Name: name },
+      cidrBlock: cidr,
+    });
 
     const publicSubnets = this.createSubnets(SubnetType.Public, opts?.publicSubnets ?? [], {
       vpcId: vpc.id,
-      availabilityZones: opts?.azs,
-      subnetTags: opts?.publicSubnetTags,
+      availabilityZones: azs,
+      subnetTags: publicSubnetTags,
     });
 
     const privateSubnets = this.createSubnets(SubnetType.Private, opts?.privateSubnets ?? [], {
       vpcId: vpc.id,
-      availabilityZones: opts?.azs,
-      subnetTags: opts?.privateSubnetTags,
+      availabilityZones: azs,
+      subnetTags: privateSubnetTags,
     });
 
-    const internetGateway = new aws.internetGateway.InternetGateway(this, 'internet-gateway', {
+    const internetGateway = new InternetGateway(this, 'internet-gateway', {
       vpcId: vpc.id,
       tags: {
         Name: `${this.opts.name}`,
-        ...opts.tags,
+        ...tags,
       },
     });
 
-    const publicRouteTable = new aws.routeTable.RouteTable(this, 'route-table-public', {
+    const publicRouteTable = new RouteTable(this, 'route-table-public', {
       vpcId: vpc.id,
       route: [
         {
@@ -42,28 +51,28 @@ export class VpcConstruct extends Construct {
           gatewayId: internetGateway.id,
         },
       ],
-      tags: { Name: `${this.opts.name}-public`, ...opts.tags },
+      tags: { Name: `${this.opts.name}-public`, ...tags },
     });
 
     // TODO Add possibility to create shared nat gateway according to singleNatGateway: true option
     const natGateways = privateSubnets.map((subnet, index) => {
-      const eip = new aws.eip.Eip(this, `eip-${index}`, {
+      const eip = new Eip(this, `eip-${index}`, {
         vpc: true,
       });
 
-      return new aws.natGateway.NatGateway(this, `nat-gateway-${index}`, {
+      return new NatGateway(this, `nat-gateway-${index}`, {
         allocationId: eip.id,
         subnetId: subnet.id,
         tags: {
           Name: `${this.opts.name}-${subnet.availabilityZone}`,
-          ...opts.tags,
+          ...tags,
         },
         dependsOn: [internetGateway],
       });
     });
 
     const privateRouteTables = privateSubnets.map((subnet, index) => {
-      return new aws.routeTable.RouteTable(this, `route-table-private-${index}`, {
+      return new RouteTable(this, `route-table-private-${index}`, {
         vpcId: vpc.id,
         route: [
           {
@@ -73,30 +82,30 @@ export class VpcConstruct extends Construct {
         ],
         tags: {
           Name: `${this.opts.name}-private-${subnet.availabilityZone}`,
-          ...opts.tags,
+          ...tags,
         },
       });
     });
 
-    const defaultRouteTable = new aws.routeTable.RouteTable(this, 'route-table-default', {
+    const defaultRouteTable = new RouteTable(this, 'route-table-default', {
       vpcId: vpc.id,
-      tags: { Name: `${this.opts.name}-default`, ...opts.tags },
+      tags: { Name: `${this.opts.name}-default`, ...tags },
     });
 
-    new aws.mainRouteTableAssociation.MainRouteTableAssociation(this, 'route-table-default-main-association', {
+    new MainRouteTableAssociation(this, 'route-table-default-main-association', {
       routeTableId: defaultRouteTable.id,
       vpcId: vpc.id,
     });
 
     publicSubnets.map((subnet, index) => {
-      new aws.routeTableAssociation.RouteTableAssociation(this, `route-table-public-association-${index}`, {
+      new RouteTableAssociation(this, `route-table-public-association-${index}`, {
         routeTableId: publicRouteTable.id,
         subnetId: subnet.id,
       });
     });
 
     privateRouteTables.map((privateRouteTable, index) => {
-      return new aws.routeTableAssociation.RouteTableAssociation(this, `route-table-private-association-${index}`, {
+      return new RouteTableAssociation(this, `route-table-private-association-${index}`, {
         routeTableId: privateRouteTable.id,
         subnetId: privateSubnets[index].id,
       });
@@ -104,7 +113,7 @@ export class VpcConstruct extends Construct {
   }
 
   private createSubnets( subnetType: SubnetType, subnetCidrs: string[], subnetOpts: SubnetOptions ) {
-    const { availabilityZones = [] } = subnetOpts;
+    const { availabilityZones = [], vpcId, subnetTags } = subnetOpts;
     const numberOfAzs = Fn.lengthOf(availabilityZones);
 
     return subnetCidrs.map((cidr, index) => {
@@ -112,14 +121,14 @@ export class VpcConstruct extends Construct {
       const subnetName = `${this.opts?.name}-${subnetType}-${availabilityZone}`;
       const subnetId = `${this.opts?.name}-${subnetType}-${index + 1}`;
 
-      return new aws.subnet.Subnet(this, subnetId, {
-        vpcId: subnetOpts.vpcId,
+      return new Subnet(this, subnetId, {
+        vpcId,
         cidrBlock: cidr,
         availabilityZone,
         mapPublicIpOnLaunch: subnetType === SubnetType.Public,
         tags: {
           Name: subnetName,
-          ...subnetOpts.subnetTags,
+          ...subnetTags,
         },
       });
     });
