393f3f068dafab0937a9acbf2cb2eedafb61c331
Mykyta Herashchenko <1991224+atre@users.noreply.github.com>
2023-12-25 14:14:09 +0100

    Feat/cdk8s basic project (#16)
    * add provider-azurerm
    
    * yarn upgrade
    
    * add remote-backend
    
    * add env interface and azure construct
    
    * update remote-backend to use AzureConstruct
    
    * add basic vn creation
    
    * extend interfaces with AzureConstructProps
    
    * prop fix
    
    * fix props for aks
    
    * increse node required version to lts
    
    * add aad provider
    
    * add provider
    
    * introduce environment entity
    
    * fix variable
    
    * fix names
    
    * add tags
    
    * add dns zone
    
    * update name
    
    * move files to tf folder
    
    * rename package
    
    * fix husky
    
    * initial cdk8s commit
    
    * move to yarn and yarn upgrade
    
    * import 1.28 schema
    
    * remove deprecated
    
    * add build to gitignore
    
    * add traefik helm
    
    * move pipeline
    
    * add dir to a pipeline
    
    * update eslint and k8s pipeline
    
    * remove exceed files

--- kubernetes/lib/traefik/index.ts
+++ kubernetes/lib/traefik/index.ts
@@ -1,20 +1,20 @@
-import { Helm, Include } from "cdk8s";
-import { Construct } from "constructs/lib/construct";
+import { Helm, Include } from 'cdk8s';
+import { Construct } from 'constructs/lib/construct';
 
 export class Traefik extends Construct {
   constructor(scope: Construct, id: string) {
-    super(scope, id)
-    
+    super(scope, id);
+
     new Include(this, 'traefik_crd_def', {
-      url: "https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml"
-    })
+      url: 'https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml',
+    });
 
     new Include(this, 'traefik_crd_rbac', {
-      url: "https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml"
-    })
+      url: 'https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml',
+    });
 
     new Helm(this, 'traefik', {
-      chart: 'traefik/traefik'
-    })
+      chart: 'traefik/traefik',
+    });
   }
 }
--- kubernetes/main.ts
+++ kubernetes/main.ts
@@ -1,5 +1,5 @@
-import { Construct } from 'constructs';
 import { App, Chart, ChartProps } from 'cdk8s';
+import { Construct } from 'constructs';
 
 export class MyChart extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
